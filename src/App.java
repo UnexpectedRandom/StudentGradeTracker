d040be24dd0ac03673a0db761b0a8e2e0e21c980
UnexpectedRandom <84254521+UnexpectedRandom@users.noreply.github.com>
2026-04-23 03:18:15 -0500

    Add command handling and database access in App
    Implement command handling for 'get' commands and add quit/help functionality.

--- src/App.java
+++ src/App.java
@@ -1,1 +1,35 @@
+import java.sql.Connection;
 
+public class App {
+    public static void main(String[] args) {
+        Connection connectSever = DatabaseConnection.getConnection();
+        boolean quit = false;
+
+        Console console = new Console();
+        DatabaseAccessor databaseManipulator = new DatabaseAccessor();
+
+        while (!quit) {
+            String command = console.getCommand();
+
+            if (command.equalsIgnoreCase("quit")) {
+                quit = true;
+            }
+            if (command.equalsIgnoreCase("help")) {
+                console.helpCommand();
+            }
+
+            if (command.toLowerCase().startsWith("get ")) {
+                if (command.substring(4).equalsIgnoreCase("student")) {
+                    databaseManipulator.getStudent(console.inputStudentID(), connectSever);
+                } else if (command.substring(4).equalsIgnoreCase("grades")) {
+                    databaseManipulator.getGrades(console.inputStudentID(), connectSever);
+                } else if(command.substring(4).equalsIgnoreCase("class")) {
+                    databaseManipulator.getClassTable(connectSever);
+                }
+            }
+        }
+
+        console.closeScanner();
+        
+    }
+}
