580494471adeaa235c9f70d06f8b65c13ed415b2
UnexpectedRandom <84254521+UnexpectedRandom@users.noreply.github.com>
2026-04-24 07:21:16 -0500

    Refactor database connection and command handling
    Refactor main method to use try-with-resources for database connection and improve command handling logic.

--- src/App.java
+++ src/App.java
@@ -1,54 +1,60 @@
 import java.sql.Connection;
-
+import java.sql.SQLException;
 public class App {
     public static void main(String[] args) {
-        Connection connectSever = DatabaseConnection.getConnection();
-        boolean quit = false;
 
-        Console console = new Console();
-        DatabaseAccessor databaseManipulator = new DatabaseAccessor();
+        try(Connection connectSever = DatabaseConnection.getConnection()) {
+            boolean quit = false;
 
-        while (!quit) {
-            String command = console.getCommand();
+            Console console = new Console();
+            DatabaseAccessor databaseManipulator = new DatabaseAccessor();
 
-            if (command.equalsIgnoreCase("quit")) {
-                quit = true;
-            }
-            if (command.equalsIgnoreCase("help")) {
-                console.helpCommand();
-            }
+            while (!quit) {
+                String command = console.getCommand();
 
-            if (command.toLowerCase().startsWith("get ")) {
-                if (command.substring(4).equalsIgnoreCase("student")) {
-                    databaseManipulator.getStudent(console.inputStudentID(), connectSever);
-                } else if (command.substring(4).equalsIgnoreCase("grades")) {
-                    databaseManipulator.getGrades(console.inputStudentID(), connectSever);
-                } else if(command.substring(4).equalsIgnoreCase("class")) {
-                    databaseManipulator.getClassTable(connectSever);
+                if (command.equalsIgnoreCase("exit")) {
+                    quit = true;
+                }
+                if (command.equalsIgnoreCase("help")) {
+                    console.helpCommand();
+                }
+
+                if (command.toLowerCase().startsWith("get ")) {
+                    if (command.substring(4).equalsIgnoreCase("student")) {
+                        databaseManipulator.getStudent(console.inputStudentID(), connectSever);
+                    } else if (command.substring(4).equalsIgnoreCase("grades")) {
+                        databaseManipulator.getGrades(console.inputStudentID(), connectSever);
+                    } else if(command.substring(4).equalsIgnoreCase("class")) {
+                        databaseManipulator.getClassTable(connectSever);
+                    }
+                }
+
+                if (command.toLowerCase().startsWith("delete ")) {
+                    if(command.substring(7).equalsIgnoreCase("student")) {
+                        databaseManipulator.deleteStudent(console.inputStudentID(), connectSever);
+                    } else if(command.substring(7).equalsIgnoreCase("grades")){
+                        databaseManipulator.deleteGrade(connectSever, console.inputStudentID(), console.InputClassID());
+                    }
+                }
+
+                if (command.toLowerCase().startsWith("add ")) {
+                    if (command.substring(4).equalsIgnoreCase("student")) {
+                        String[] studentInfo = console.studentInformation();
+                        databaseManipulator.addStudent(connectSever, studentInfo[0], studentInfo[1], studentInfo[2]);
+                    } else if(command.substring(4).equalsIgnoreCase("grades")) {
+                        
+                        Console.gradeEntry gradeInfo = console.gradeInfo();
+                        databaseManipulator.addGrade(connectSever, gradeInfo.studentID(),gradeInfo.feedback() , gradeInfo.grade(), gradeInfo.classID());
+                    }
                 }
             }
-
-            if (command.toLowerCase().startsWith("delete ")) {
-                if(command.substring(7).equalsIgnoreCase("student")) {
-                    databaseManipulator.deleteStudent(console.inputStudentID(), connectSever);
-                } else if(command.substring(7).equalsIgnoreCase("grades")){
-                    databaseManipulator.deleteGrade(connectSever, console.inputStudentID(), console.InputclassID());
-                }
-            }
-
-            if (command.toLowerCase().startsWith("add ")) {
-                if (command.substring(4).equalsIgnoreCase("student")) {
-                    String[] studentInfo = console.studentInformation();
-                    databaseManipulator.addStudent(connectSever, studentInfo[0], studentInfo[1], studentInfo[2]);
-                } else if(command.substring(4).equalsIgnoreCase("grades")) {
-                    
-                    Console.gradeEntry gradeInfo = console.gradeInfo();
-                    databaseManipulator.addGrade(connectSever, gradeInfo.studentID(),gradeInfo.feedback() , gradeInfo.grade(), gradeInfo.classID());
-                }
-            }
+            console.closeScanner();
+        } catch (SQLException e) {
+            System.out.println("Error in Database");
+            e.printStackTrace();
         }
-
-        console.closeScanner();
         
+        
+
     }
 }
