849b9c7364d3b0457889545a0afa3625f972dd50
UnexpectedRandom <84254521+UnexpectedRandom@users.noreply.github.com>
2026-04-24 00:44:14 -0500

    Add delete and add commands for student and grades

--- src/App.java
+++ src/App.java
@@ -27,6 +27,25 @@ public class App {
                     databaseManipulator.getClassTable(connectSever);
                 }
             }
+
+            if (command.toLowerCase().startsWith("delete ")) {
+                if(command.substring(7).equalsIgnoreCase("student")) {
+                    databaseManipulator.deleteStudent(console.inputStudentID(), connectSever);
+                } else if(command.substring(7).equalsIgnoreCase("grades")){
+                    databaseManipulator.deleteGrade(connectSever, console.inputStudentID(), console.InputclassID());
+                }
+            }
+
+            if (command.toLowerCase().startsWith("add ")) {
+                if (command.substring(4).equalsIgnoreCase("student")) {
+                    String[] studentInfo = console.studentInformation();
+                    databaseManipulator.addStudent(connectSever, studentInfo[0], studentInfo[1], studentInfo[2]);
+                } else if(command.substring(4).equalsIgnoreCase("grades")) {
+                    
+                    Console.gradeEntry gradeInfo = console.gradeInfo();
+                    databaseManipulator.addGrade(connectSever, gradeInfo.studentID(),gradeInfo.feedback() , gradeInfo.grade(), gradeInfo.classID());
+                }
+            }
         }
 
         console.closeScanner();
